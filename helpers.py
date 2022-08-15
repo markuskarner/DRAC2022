0dff7e7b1607c1daf82eb931718ac0b85b035f7d
markuskarner <markus.karner1994@gmail.com>
2022-08-15 16:40:27 +0200

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,232 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+import torchvision.transforms as transforms
+import wandb
+
+from PIL import Image
+from torchvision import models
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+from torch import nn, optim
+from sklearn.metrics import balanced_accuracy_score
+
+
+class DracClassificationDatasetTrain(Dataset):
+
+    def __init__(self,
+                 images_folder: str,
+                 labels_csv: str,
+                 transform: transforms.Compose):
+        self.labels = pd.read_csv(labels_csv)
+        self.img_path = images_folder
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img_file = self.labels.iloc[idx, 0]
+        label = self.labels.iloc[idx, 1]
+
+        image = np.array(Image.open(self.img_path + img_file))
+        image = np.repeat(image[..., np.newaxis], 3, axis=2)
+
+        return self.transform(image), label
+
+    @property
+    def targets(self):
+        return self.labels
+
+
+class DracClassificationDatasetTest(Dataset):
+
+    def __init__(self,
+                 images_folder: str,
+                 transform: transforms.Compose):
+        self.img_path = images_folder
+        self.images = [file for file in os.listdir(images_folder)]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_file = self.images[idx]
+        img_id = int(img_file.split('.')[0])
+
+        image = np.array(Image.open(self.img_path + img_file))
+        image = np.repeat(image[..., np.newaxis], 3, axis=2)
+
+        return self.transform(image), img_id
+
+
+class DracClassificationModel(nn.Module):
+    """ Variant of a pretrained network from torchvision
+    for classifying images from the Drac2022 dataset.
+    """
+
+    # TODO down sample input images
+
+    def __init__(self, features: nn.Module,
+                 num_classes: int = 3,
+                 flattened_size: int = 1000,
+                 dropout: float = 0.):
+        """
+        Parameters
+        ----------
+        features : nn.Module
+            The convolutional part of a pretrained network.
+        num_classes : int
+            The number of output classes in the data.
+        flattened_size: int
+            Size of the flattened layer after AdaptiveAvgPool2d
+        """
+        super(DracClassificationModel, self).__init__()
+
+        # self.downsampler = nn.Sequential(
+        #     nn.Conv2d()
+        # )
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(in_features=flattened_size, out_features=num_classes)
+        )
+
+        # init classifier
+        for m in self.classifier.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+def init_model(dropout: float = 0.):
+    # TODO replace with convNext
+    resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+
+    return DracClassificationModel(resnet50,
+                                   flattened_size=1000,
+                                   dropout=dropout)
+
+
+@torch.no_grad()
+def evaluate(network: nn.Module, data: DataLoader, metric: callable) -> list:
+    network.eval()
+    errors = []
+
+    device = next(network.parameters()).device
+
+    for batch_idx, (x, y) in enumerate(data):
+        x, y = x.float().to(device), y.to(device)
+
+        y_hat = network(x)
+        errors.append(metric(y_hat, y).item())
+
+        # TODO add logging for the necessary metrics of DRAC2022
+        acc = balanced_accuracy_score(y.cpu(), torch.argmax(y_hat, dim=1).cpu().detach().numpy())
+        wandb.log({"validation accuracy": acc})
+
+    return errors
+
+
+@torch.enable_grad()
+def update(network: nn.Module, data: DataLoader, loss: nn.Module,
+           opt: optim.Optimizer) -> list:
+    network.train()
+    errors = []
+
+    device = next(network.parameters()).device
+
+    for batch_idx, (x, y) in enumerate(data):
+        x, y = x.float().to(device), y.to(device)
+
+        y_hat = network(x)
+        output = loss(y_hat, y)
+        output.backward()
+        opt.step()
+        opt.zero_grad()
+
+        errors.append(output.item())
+
+        # TODO add logging for the necessary metrics of DRAC2022
+        acc = balanced_accuracy_score(y.cpu(), torch.argmax(y_hat, dim=1).cpu().detach().numpy())
+        wandb.log({"train accuracy": acc})
+
+    return errors
+
+
+def prepare_transform(base_path: str, image_folder: str, calculate_mean_and_std: bool = False):
+
+    if calculate_mean_and_std:
+        images = image_folder  # b_x_train_raw_path
+        ids = set()
+        for file in os.listdir(images):
+            ids.add(file)
+
+        mean = 0
+        std = 0
+        for i in ids:
+            img_tensor = transforms.ToTensor()(Image.open(images + i))
+
+            mean += 1 / len(ids) * torch.mean(img_tensor, (1, 2))
+            std += 1 / len(ids) * torch.std(img_tensor, (1, 2))
+
+            torch.save(mean, base_path + "mean.pt")
+            torch.save(std, base_path + "std.pt")
+    else:
+        mean = torch.load(base_path + "mean.pt")
+        std = torch.load(base_path + "std.pt")
+
+    print(f'mean:{mean}, std:{std}')
+
+    transform = {
+        "train": transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Resize([256, 256]),
+             transforms.RandomCrop(224),
+             transforms.RandomHorizontalFlip(),
+             transforms.Normalize(mean=mean,
+                                  std=std)]),
+        "test": transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Resize([224, 224]),
+             transforms.Normalize(mean=mean,
+                                  std=std)])
+    }
+
+    return transform
+
+
+def prepare_classification_dataset(base_path: str,
+                                   image_folder: str,
+                                   labels_csv: str,
+                                   batch_size: int,
+                                   num_workers: int = 4):
+
+    transform = prepare_transform(base_path, image_folder, False)
+
+    data_train_valid = DracClassificationDatasetTrain(image_folder, labels_csv, transform["train"])
+    data_train, data_valid = torch.utils.data.random_split(data_train_valid, [600, 65])
+
+    # Weighted sampling for train data
+    targets = pd.read_csv(labels_csv)
+    train_target = targets.iloc[data_train.indices, 1]
+    train_class_sample_count = np.array(
+        [len(np.where(train_target == t)[0]) for t in np.unique(train_target)])
+    train_weight = 1. / train_class_sample_count
+
+    train_samples_weight = np.array([train_weight[t] for t in train_target])
+
+    train_samples_weight = torch.from_numpy(train_samples_weight).double()
+
+    train_sampler = WeightedRandomSampler(train_samples_weight, len(train_samples_weight))
+
+    # dataloader_train = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    dataloader_train = DataLoader(data_train, batch_size=batch_size, num_workers=num_workers, sampler=train_sampler)
+    dataloader_valid = DataLoader(data_valid, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    return dataloader_train, dataloader_valid
---
+++ main.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import torch
+import wandb
+
+from torch import nn
+from torch import optim
+
+
+from helpers import init_model, update, evaluate, prepare_classification_dataset
+
+if __name__ == "__main__":
+
+    TASK = 'b'  # for now only b and c work (classification)
+
+    # Prepare paths
+    if TASK == 'a':
+        base_path = "/Users/markus/Downloads/DRAC2022/A. Segmentation/"
+        x_train_raw_path = base_path + "/1. Original Images/a. Training Set/"
+        y_train_raw_path = base_path + "/2. Groundtruths/a. Training Set/"
+    elif TASK == 'b':
+        base_path = "/Users/markus/Downloads/DRAC2022/B. Image Quality Assessment/"
+        x_train_raw_path = base_path + "/1. Original Images/a. Training Set/"
+        y_train_raw_path = base_path + "/2. Groundtruths/a. DRAC2022_ Image Quality Assessment_Training Labels.csv"
+    elif TASK == 'c':
+        base_path = "/Users/markus/Downloads/DRAC2022/C. Diabetic Retinopathy Grading/"
+        x_train_raw_path = base_path + "/1. Original Images/a. Training Set/"
+        y_train_raw_path = base_path + "/2. Groundtruths/" \
+                                         "a. DRAC2022_ Diabetic Retinopathy Grading_Training Labels.csv"
+    else:
+        raise Exception("Only Tasks a,b or c allowed!")
+
+    labels = pd.read_csv(y_train_raw_path)
+
+    sweep_config = {
+        "method": "random",
+        "metric": {
+            "name": "accuracy",
+            "goal": "maximize"
+        },
+        "parameters": {
+            "model": {
+                "values": ["ResNet50"]  # TODO change to ConvNeXt after changing init model
+            },
+            "task": {
+                "values": ["Classification B Quality"]
+            },
+            "epochs": {
+                "values": [20, 30]
+            },
+            "learning_rate": {
+                "min": 0.00001,
+                "max": 0.1
+            },
+            "weight_decay": {
+                "values": [0.0005, 0.005, 0.05]
+            },
+            "batch_size": {
+                "values": [16, 32, 64, 128]
+            },
+            "dropout": {
+                "values": [0.]  # [0., 0.3, 0.4, 0.5]
+            },
+        }
+    }
+
+    def train():
+        with wandb.init():
+            config = wandb.config
+
+            model = init_model(config.dropout)
+            device = torch.device("mps")
+            model.to(device)
+
+            dataloader_train, dataloader_valid = prepare_classification_dataset(base_path,
+                                                                                x_train_raw_path,
+                                                                                y_train_raw_path,
+                                                                                config["batch_size"])
+
+            opt = optim.Adam(model.parameters(),
+                             lr=config["learning_rate"],
+                             weight_decay=config["weight_decay"])
+            ce = nn.CrossEntropyLoss()
+
+            for epoch in range(config["epochs"]):
+                local_errs = update(model, dataloader_train, ce, opt)
+                local_val_errs = evaluate(model, dataloader_valid, ce)
+
+                wandb.log({"train error": sum(local_errs) / len(local_errs),
+                           "validation error": sum(local_val_errs) / len(local_val_errs)})
+
+
+    continue_sweep_id = None
+
+    if not continue_sweep_id:
+        sweep_id = wandb.sweep(sweep_config, entity="markuskarner", project="DRAC2022")
+
+        count = 10  # number of runs to execute
+        wandb.agent(sweep_id, function=train, count=count)
+    else:
+        wandb.agent(continue_sweep_id, function=train, project="DRAC2022")
